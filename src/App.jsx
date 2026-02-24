8ace3f2c6029ed5c6f6397438875a872d67f5bc6
Guillermo Acquistapace <guillermoacq@gmail.com>
2026-02-24 11:39:51 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <main >
+        {/* title */}
+        <section></section> 
+
+        {/* about */}
+        <section></section>
+
+        {/* my projects */}
+        <section></section>
+
+        {/* blog */}
+        <section></section>
+
+        {/* contact */}
+
+      </main>
+    </>
+  )
+}
+
+export default App
