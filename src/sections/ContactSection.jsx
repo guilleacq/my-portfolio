b6adcb7e4dba62e9e709ec7653b759605b01cf2a
Guillermo Acquistapace <guillermoacq@gmail.com>
2026-02-24 14:24:47 -0300

    Contact section improved

--- src/sections/ContactSection.jsx
+++ src/sections/ContactSection.jsx
@@ -1,16 +1,17 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import CustomLink from '../components/CustomLink';
 
 const links = [
-  { text: "github", href: "https://github.com/test", icon: <FaGithub /> },
-  { text: "linkedin", href: "https://linkedin.com/in/test", icon: <FaLinkedin /> },
+    { href: "mailto:me@gmail.com", icon: <FaEnvelope /> },
+    { href: "https://github.com/test", icon: <FaGithub /> },
+    { href: "https://linkedin.com/in/test", icon: <FaLinkedin /> },
 ];
 
 const ContactSection = () => {
     return (
         <section className="contact-section">
         {links.map(link => (
-            <CustomLink key={link.text} icon={link.icon} text={link.text} href={link.href} />
+            <CustomLink key={link.href} icon={link.icon} text={link.text} href={link.href} />
         ))}
         </section>
     );
