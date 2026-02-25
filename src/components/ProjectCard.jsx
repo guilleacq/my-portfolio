049c260e032f686d88df1451b54373f7c5a1f879
Guillermo Acquistapace <guillermoacq@gmail.com>
2026-02-25 14:33:45 -0300

    Fixed compatibility issues

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,10 @@
-const ProjectCard = ({ project = null }) => {
+ 
+
+const ProjectCard = ({ title, description }) => {
     return (
-        <div className="project-card">
-            <h3 className="text-2xl font-bold text-stone-200">Test</h3>
-            <p className="text-stone-300 mt-2">Este es un test lol</p>
-            <div className="flex mt-4 gap-4">
-                {project.links.map((link, index) => (
-                    <a 
-                        key={index} 
-                        href={link.url} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        className="text-blue-500 hover:underline"
-                    >
-                        {link.label}
-                    </a>
-                ))}
-            </div>
+        <div className="project-card bg-red-500">
+            <h3 className="text-2xl font-bold text-stone-200">{title}</h3>
+            <p className="text-stone-300 mt-2">{description}</p>
         </div>
     )
 };
--- src/sections/ProjectsSection.jsx
+++ src/sections/ProjectsSection.jsx
@@ -1,10 +1,35 @@
 import TitledSection from "../components/TitledSection.jsx";
+import ProjectCard from "@/components/ProjectCard.jsx";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel";
+
+// I will load these from outside later
+const projects = [
+{ title: "Project One", description: "A cool thing I built" },
+{ title: "Project Two", description: "Another cool thing" },
+{ title: "Project Three", description: "Yet another one" },
+];
+
 
 const ProjectsSection = () => {
     return (
         <section>
             <TitledSection title={"my_projects"}>
-                <p>Ses</p>
+                <Carousel>
+                    <CarouselContent>
+                        {projects.map((project) => (
+                            <CarouselItem key={project.title}>
+                                <ProjectCard title={project.title} description={project.description}/>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+
+                </Carousel>
             </TitledSection>
         </section>
     );
