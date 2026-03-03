c3e21aae7f63e98943fc60f69b4f46b16031c620
guilleacq <g.acquistapace@correo.ucu.edu.uy>
2026-03-03 16:07:32 -0300

    Improved homescreen view

--- src/components/GoHomeArrow.jsx
+++ src/components/GoHomeArrow.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 const GoHomeArrow = () => {
     return (
         <>
-            <Link to="/" className="flex flex-row items-center gap-2 w-fit p-2 decoration-accent hover:underline" aria-label={`Go home`}>
+            <Link to="/" className="flex flex-row items-center gap-2 w-fit pr-4 py-2 decoration-accent hover:underline" aria-label={`Go home`}>
                 <FaArrowLeft className="text-accent text-xs"/>
                 <span className="text-sm text-accent leading-none">back</span>
             </Link>
--- src/components/TitledSection.jsx
+++ src/components/TitledSection.jsx
@@ -1,11 +1,11 @@
-const TitledSection = ({title, children}) => {
+const TitledSection = ({title, children, noPadding = false}) => {
     return (
         <div>
             <div className="text-xs text-muted-foreground flex items-center gap-2 font-inter">
                 <span className="text-accent">{'>'}</span>
                 {title}
             </div>
-            <div className="mt-4 pl-4">
+            <div className={`mt-4 ${noPadding ? '' : 'pl-4'}`}>
                 {children}
             </div>
         </div>
