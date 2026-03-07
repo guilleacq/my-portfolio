2de6600ecbeb008c862b517f142073563610bca4
guilleacq <g.acquistapace@correo.ucu.edu.uy>
2026-03-07 17:52:32 -0300

    minor quality of life tweaks

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,6 +6,8 @@ const Layout = () => {
         <main className="flex flex-col gap-6">
             <Outlet />
         </main>
+        {/* Empty space for visual spacing */}
+        <div className="h-32" aria-hidden="true" /> 
     </div>
   );
 }
